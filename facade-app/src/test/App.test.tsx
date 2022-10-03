0ba546eca6fea78bdc9b90d4968a9a1cc1c7db83
lejara <leoneljara3@gmail.com>
2022-10-03 14:15:18 -0400

    Undo revert

--- facade-app/src/test/App.test.tsx
+++ facade-app/src/test/App.test.tsx
@@ -31,7 +31,7 @@ describe("App", () => {
   };
 
   it("renders correctly", async () => {
-    expect(render(<App />).container).toMatchSnapshot();
+    expect(_renderApp().container).toMatchSnapshot();
   });
 
   // it("renders left nav container", () => {
